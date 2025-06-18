99c0c2bb44fc214db17e3da8782abb93fe0b1a1a
tjanthaw <thananyajanthawong@gmail.com>
2025-06-18 06:21:52 +0000

    201

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,13 @@
+namber = int(input())
+if namber > 0:
+	print("This number is positive.")
+if namber < 0:
+	print("This number is negative.")
+else:
+	print("This number is both positive and negative.")
+	
+	
+	
+	
+														
+                                            
