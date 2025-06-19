9907518c9526c19c9414485f637d98b5869be1c2
tjanthaw <thananyajanthawong@gmail.com>
2025-06-19 18:13:36 +0000

    501

--- cell05/ex00/create_array.py
+++ cell05/ex00/create_array.py
@@ -1,8 +1,5 @@
 
-def main():
-    array = [2, 8, 9, 48, 8, 22, -12, 2]
-    print(array)
+    arr = [2, 8, 9, 48, 8, 22, -12, 2]
+    print(arr)
 
-if __name__ == "__main__":
-    main()
     
---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+def main():
+    original_array = [2,8,9,48,8,22,-12,2]
+    new_array = [x + 2 for x in originai_array]
+
+    print("Originai array:", originai_array)
+    print("new array:"new_array)
+
+if __name__== "__main__":
+    main()
